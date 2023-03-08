26611b0c1c107508a2ebf661baa86d5e0ce99177
Umar Siddiq <umar.khoiruddin@binus.ac.id>
2023-03-08 16:54:31 +0700

    Data Diri Umar

--- resources/js/script.js
+++ resources/js/script.js
@@ -9,11 +9,11 @@ let dataDiri = [
   },
   {
     nama: 'Umar Siddiq Gege Khoiruddin',
-    wanted: '',
-    summary: '',
-    hobby: '',
-    matkul: '',
-    gambar: ''
+    wanted: '$100.000',
+    summary: 'Hello, my name is Umar Siddiq Gege Khoiruddin, now I m studying at Bina Nusantara University majoring in computer science and statistics since 2020 ',
+    hobby: ['Membaca buku'],
+    matkul: ['Computer network'],
+    gambar: 'pics/umar.jpg'
   },
   {
     nama: 'Sekar Azalea',
