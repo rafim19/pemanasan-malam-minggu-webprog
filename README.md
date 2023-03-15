ad7f8d15dee471fddad801ad6288121733e60f30
Rafi Muhammad <rafi.muhammad001@binus.ac.id>
2023-03-15 11:53:50 +0700

    styling for mobile page

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Documentation
-## Home Page:
-![Profile Page](public/pics/documentation/Home_page.png)
+## Desktop Viewport:
+1. Home Page:
+![Home Desktop Page](public/pics/documentation/Home_page.png)
 
-## Profile Page:
-![Profile Page](public/pics/documentation/Profile_page.png)
+2. Profile Page:
+![Profile Desktop Page](public/pics/documentation/Profile_page.png)
+
+## Mobile Viewport:
+1. Home Page
+![Home Mobile Page](public/pics/documentation/home_mobile.png)
+2. Profile Page
+![Profile Mobile Page](public/pics/documentation/Profile_mobile.png)
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,11 @@
 @section('title', 'Home')
 
 @section('main')
-    <div class="is-flex is-justify-content-space-between is-align-items-center container is-fluid" style="color: white;">
+    <div class="is-flex is-justify-content-space-between is-align-items-center container is-fluid all-content" style="color: white;">
         <div class="column is-flex is-flex-direction-column is-align-items-center is-justify-content-start mt-5" style="height: 100%;">
             <img class="member-pic" src="{{ asset('pics/rafi.jpg') }}" alt="">
-            <h1 class="is-size-4"><b>Rafi Muhammad</b></h1>
-            <div class="about-us">
+            <h1 class="is-size-4 mb-3"><b>Rafi Muhammad</b></h1>
+            <div class="is-flex is-flex-direction-column about-us">
                 <p>Hello, my name is Rafi Muhammad. I am in my 20s and always eager to learn anything that takes my attention. Currently, I am studying computer science and statistics at Bina Nusantara University. In addition, I am interested in Full Stack Web Development & Mobile Development</p>
                 <p class="mt-5">Hobby:</p>
                 <ul>
@@ -23,14 +23,14 @@
                     <li class="ml-4">Web Programming</li>
                     <li class="ml-4">Software Engineering</li>
                 </ul>
+                <a href="{{ route('profile', 0) }}" id="profile-rafi" class="button is-warning is-rounded mt-5 mb-5 is-align-self-center" type="button">Profile</a>
             </div>
-            <div><a href="{{ route('profile', 0) }}" id="profile-rafi" class="button is-warning is-rounded mt-5 mb-5" type="button">Profile</div></a>
             {{-- <a class="button is-warning is-rounded mt-5 mb-5" href="">Profile</a> --}}
         </div>
         <div class="column is-flex is-flex-direction-column is-align-items-center is-justify-content-start mt-5" style="height: 100%;">
             <img class="member-pic" src="{{ asset('pics/sekar.jpg') }}" alt="">
-            <h1 class="is-size-4"><b>Sekar Azalea</b></h1>
-            <div class="about-us">
+            <h1 class="is-size-4 mb-3"><b>Sekar Azalea</b></h1>
+            <div class="is-flex is-flex-direction-column about-us">
                 <p class="summary">Hello, my name is Sekar Azalea. I am in my 20s and always eager to learn anything. Adaptive is one word that describes me the most. Currently, I am studying computer science and statistics at Bina Nusantara University. In addition, I am interested in designing UI/UX and back-end engineering.</p>
                 <p class="mt-5">Hobby:</p>
                 <ul>
@@ -40,14 +40,14 @@
                 <ul>
                     <li class="ml-4">Human and Computer Interaction</li>
                 </ul>
+                <a href="{{ route('profile', 1) }}"id="profile-sekar" class="button is-warning is-rounded mt-5 mb-5 is-align-self-center" type="button">Profile</a>
             </div>
-            <a href="{{ route('profile', 1) }}"id="profile-sekar" class="button is-warning is-rounded mt-5 mb-5" type="button">Profile</a>
             {{-- <a class="button is-warning is-rounded mt-5 mb-5" href="">Profile</a> --}}
         </div>
         <div class="column is-flex is-flex-direction-column is-align-items-center is-justify-content-start mt-5" style="height: 100%;">
             <img class="member-pic" src="{{ asset('pics/umar.jpg') }}" alt="">
-            <h1 class="is-size-4"><b>Umar Siddiq</b></h1>
-            <div class="about-us">
+            <h1 class="is-size-4 mb-3"><b>Umar Siddiq</b></h1>
+            <div class="is-flex is-flex-direction-column about-us">
                 <p class="summary">Hello, my name is Umar Siddiq Gege Khoiruddin, now I m studying at Bina Nusantara University majoring in computer science and statistics since 2020</p>
                 <p class="mt-5">Hobby:</p>
                 <ul>
@@ -57,8 +57,8 @@
                 <ul>
                     <li class="ml-4">Computer Network</li>
                 </ul>
+                <a href="{{ route('profile', 2) }}" id="profile-umar" class="button is-warning is-rounded mt-5 mb-5 is-align-self-center" type="button">Profile</a>
             </div>
-            <a href="{{ route('profile', 2) }}" id="profile-umar" class="button is-warning is-rounded mt-5 mb-5" type="button">Profile</a>
             {{-- <a class="button is-warning is-rounded mt-5 mb-5" href="">Profile</a> --}}
         </div>
     </div>
